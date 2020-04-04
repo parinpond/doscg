bfcbb0797070f38b2cc22aac214c791c9ab55819
parinpond <parinpond.lag@gmail.com>
2020-04-05 00:23:25 +0700

    chat,map

--- public/js/doscg/map.js
+++ public/js/doscg/map.js
@@ -1,9 +1,35 @@
-var map;
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
+    var myLatLng = { lat: 13.753352, lng: 100.543626 };
 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: { lat: 13.787237, lng: 100.533829 },
+        mapTypeId: 'terrain',
+
+    });
+    var triangleCoords = [
+        { lat: 13.7466304, lng: 100.5371464 },
+        { lat: 13.80579, lng: 100.537635 }
+    ];
+
+    var bermudaTriangle = new google.maps.Polygon({
+        paths: triangleCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
+    bermudaTriangle.setMap(map);
+
+    var beachMarker = new google.maps.Marker({
+        position: { lat: 13.7466304, lng: 100.5371464 },
+        map: map,
+        label: "Centralworld",
+    });
+    var beachMarker = new google.maps.Marker({
+        position: { lat: 13.80579, lng: 100.537635 },
+        map: map,
+        label: "SCG",
+    });
 }
