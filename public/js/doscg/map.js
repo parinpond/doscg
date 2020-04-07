86c69d4ef1c55d8735398fa6fa8444f39e0bc83e
Parinyimz <40853593+parinpond@users.noreply.github.com>
2020-04-07 20:56:26 +0700

    Update map.js
    space

--- public/js/doscg/map.js
+++ public/js/doscg/map.js
@@ -1,17 +1,14 @@
 function initMap() {
     var myLatLng = { lat: 13.753352, lng: 100.543626 };
-
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: { lat: 13.787237, lng: 100.533829 },
         mapTypeId: 'terrain',
-
     });
     var triangleCoords = [
         { lat: 13.7466304, lng: 100.5371464 },
         { lat: 13.80579, lng: 100.537635 }
     ];
-
     var bermudaTriangle = new google.maps.Polygon({
         paths: triangleCoords,
         strokeColor: '#FF0000',
@@ -21,7 +18,6 @@ function initMap() {
         fillOpacity: 0.35
     });
     bermudaTriangle.setMap(map);
-
     var beachMarker = new google.maps.Marker({
         position: { lat: 13.7466304, lng: 100.5371464 },
         map: map,
